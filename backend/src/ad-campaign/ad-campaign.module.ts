e055ad7b2fb6a752c7b11a1f02cddbe229618930
gsyou1979 <gsyou1979@gmail.com>
2026-05-18 19:05:52 +0900

    fix: add AdItem and AdCreative to AdCampaignModule imports

--- backend/src/ad-campaign/ad-campaign.module.ts
+++ backend/src/ad-campaign/ad-campaign.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AdCampaign } from './entities/ad-campaign.entity';
 import { AdCampaignService } from './ad-campaign.service';
 import { AdCampaignController } from './ad-campaign.controller';
+import { AdItem } from '../ad-item/entities/ad-item.entity';
+import { AdCreative } from '../ad-creative/entities/ad-creative.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AdCampaign])],
+  imports: [TypeOrmModule.forFeature([AdCampaign, AdItem, AdCreative])],
   controllers: [AdCampaignController],
   providers: [AdCampaignService],
   exports: [AdCampaignService],
